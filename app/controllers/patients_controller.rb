768f407ab97c3817cb66e6818803b02b037bcc2e
github username <github email address>
2019-04-10 13:50:20 -0700

    Done.

---
+++ app/controllers/patients_controller.rb
@@ -0,0 +1,11 @@
+class PatientsController < ApplicationController
+
+  def show
+    @patient = Patient.find(params[:id])
+    @appointments = @patient.appointments
+  end
+
+  def index
+    @patients = Patient.all
+  end
+end
